eae2fb1c354a099c8001fde47f3fb4bcc87cb8f9
ChloeLee01 <youngsunyi01@gmail.com>
2024-11-06 15:06:32 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const PORT = 8000;
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express(); // express 객체를 app으로 저장해 사용
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World Tasks App Node Test Root"); // .send 브라우저에 찍힘
+});
+
+app.use(require('./routes/getRoutes'));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
